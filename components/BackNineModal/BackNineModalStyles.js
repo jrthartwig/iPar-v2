9b5bb1693dfe0e449a9535a57c1a12b05b6e1209
Jocelynn Hartwig <jocelynnhartwig@gmail.com>
2020-06-24 15:29:10 -0400

    fix: styling consolidation

--- components/BackNineModal/BackNineModalStyles.js
+++ components/BackNineModal/BackNineModalStyles.js
@@ -1,120 +1,90 @@
-import {StyleSheet} from 'react-native';
+import { StyleSheet } from "react-native";
+import titleTextColors from './titleTextColors';
 
-const styles = StyleSheet.create({
-    modal: {
-      justifyContent: "center",
-      alignItems: "center",
-      flexDirection: "row",
-      position: "absolute",
-      top: 0,
-      left: 0,
-      right: 0,
-      bottom: 40
-    },
-    title: {
-      flexDirection: "column"
-    },
-    titleText: {
-      padding: 6.5,
-      fontSize: 14,
-      fontWeight: "bold",
-    },
-    holeTitleText: {
-      padding: 6.5,
-      fontSize: 14,
-      fontWeight: "bold",
-      backgroundColor: "#ddd",
-    },
-    talonTitleText: {
-      padding: 6.5,
-      fontSize: 14,
-      fontWeight: "bold",
-      backgroundColor: "black",
-      color: 'white'
-    },
-    augustaTitleText: {
-      padding: 6.5,
-      fontSize: 14,
-      fontWeight: "bold",
-      backgroundColor: "#006633",
-      color: 'white'
-    },
-    whiteTitleText: {
-      padding: 6.5,
-      fontSize: 14,
-      fontWeight: "bold",
-      backgroundColor: "#666666",
-      color: 'white'
-    },
-    redtailTitleText: {
-      padding: 6.5,
-      fontSize: 14,
-      fontWeight: "bold",
-      backgroundColor: "#990033",
-      color: 'white'
-    }, 
-    seniorTitleText: {
-      padding: 6.5,
-      fontSize: 14,
-      fontWeight: "bold",
-      backgroundColor: "#999900",
-      color: 'white'
-    },
-    holeNumbers: {
-      justifyContent: "space-between",
-      padding: 6.5,
-      fontSize: 14,
-    },
-    topHoleNumbers: {
-      justifyContent: "space-between",
-      padding: 6.5,
-      fontSize: 14,
-      backgroundColor: "#ddd",
-    },
-    talonYardage: {
-      justifyContent: "space-between",
-      padding: 6.5,
-      fontSize: 14,
-      backgroundColor: 'black',
-      color: 'white' 
-    },
-    augustaYardage: {
-      justifyContent: "space-between",
-      padding: 6.5,
-      fontSize: 14,
-      backgroundColor: '#006633',
-      color: 'white' 
-    },
-    whiteYardage: {
-      justifyContent: "space-between",
-      padding: 6.5,
-      fontSize: 14,
-      backgroundColor: '#666666',
-      color: 'white' 
-    },
-    redtailYardage: {
-      justifyContent: "space-between",
-      padding: 6.5,
-      fontSize: 14,
-      backgroundColor: '#990033',
-      color: 'white' 
-    }, 
-    seniorYardage: {
-      justifyContent: "space-between",
-      padding: 6.5,
-      fontSize: 14,
-      backgroundColor: '#999900',
-      color: 'white'
-    },
-    button: {
-      position: "absolute",
-      top: 0,
-      left: 0,
-      right: 0,
-      bottom: -300,
-      justifyContent: "center",
-      alignItems: "center"
-    }
-})
+function createTitleText(backgroundColor) {
+  return {
+    padding: 6.5,
+    fontSize: 14,
+    fontWeight: "bold",
+    backgroundColor,
+    color: "white",
+  };
+}
 
-export default styles; 
+const titleTextStyles = {};
+titleTextColors.forEach((titleTextColor) => {
+  titleTextStyles[`${titleTextColor.title}TitleText`] = createTitleText(
+    titleTextColor.color
+  );
+});
+
+
+function createYardageTitleText(backgroundColor) {
+  return {
+    justifyContent: "space-between",
+    padding: 6.5,
+    fontSize: 14,
+    backgroundColor,
+    color: "white"
+  }
+}
+
+const yardageTitleTextStyles = {};
+titleTextColors.forEach((titleTextColor) => {
+  yardageTitleTextStyles[`${titleTextColor.title}Yardage`] = createYardageTitleText(
+    titleTextColor.color
+  );
+});
+
+let backNineModalStyles = {
+  modal: {
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 40,
+  },
+  title: {
+    flexDirection: "column",
+  },
+  titleText: {
+    padding: 6.5,
+    fontSize: 14,
+    fontWeight: "bold",
+  },
+  holeTitleText: {
+    padding: 6.5,
+    fontSize: 14,
+    fontWeight: "bold",
+    backgroundColor: "#ddd",
+  },
+  holeNumbers: {
+    justifyContent: "space-between",
+    padding: 6.5,
+    fontSize: 14,
+  },
+  topHoleNumbers: {
+    justifyContent: "space-between",
+    padding: 6.5,
+    fontSize: 14,
+    backgroundColor: "#ddd",
+  },
+  button: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: -300,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+};
+
+const styles = StyleSheet.create(backNineModalStyles);
+
+backNineModalStyles = { ...backNineModalStyles, ...titleTextStyles, ...yardageTitleTextStyles };
+
+export default styles;
---
+++ components/BackNineModal/titleTextColors.js
@@ -0,0 +1,24 @@
+const titleTextColors = [
+    {
+      title: "talon",
+      color: "black",
+    },
+    {
+      title: "white",
+      color: "#666",
+    },
+    {
+      title: "augusta",
+      color: "#006633",
+    },
+    {
+      title: "redtail",
+      color: "#990033"
+    },
+    {
+      title: "senior",
+      color: "#999900"
+    }
+  ];
+
+  export default titleTextColors;
